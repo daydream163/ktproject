aa2509c94eb8e0be668730dd460046c26f0d8f1d
daydream163 <daydream@163.com>
2020-11-20 16:47:22 +0800

    登录、退出处理

---
+++ KTProject.Common/Log/Log4Helper.cs
@@ -0,0 +1,20 @@
+﻿using log4net;
+
+namespace KTProject.Common
+{
+	/// <summary>
+	/// 日志类
+	/// </summary>
+	public class Log4Helper
+	{
+		private static readonly ILog logInfo = LogManager.GetLogger("WebInfoLogger");
+
+		public static void Info(object message)
+		{
+			if (logInfo.IsInfoEnabled)
+			{
+				logInfo.Info(message);
+			}
+		}
+	}
+}
